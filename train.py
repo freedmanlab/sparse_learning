b2242c04226075e1c12a4b8a3e29356fb06a8418
nymasse <nicolas.masse.github@gmail.com>
2023-05-10 21:33:08 -0400

    major bug fix

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ from tasks import SoftmaxCrossEntropy, ActorCritic
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from constants import TASKS, train_params, model_params
+from pytorch_lightning.loggers import CSVLogger
+
 
 class Train:
 
@@ -16,6 +18,7 @@ class Train:
 
         # create a dummy loader in order to extract the stimulus properties
         dummy_loader = TaskDataset(tasks=None, n_batches=0, RL=False)
+        self.logger = CSVLogger("logs", name="my_exp_name")
 
         # if network is "LSTM" or "RNN", then initialize the network
         # else, we'll use the network (nn.Module) provided
@@ -29,20 +32,27 @@ class Train:
         """Create the network optimizer. The learning rate of network parameters not involved in top-down
         context will be set to 0 if sparse_plasticity = True"""
 
-        top_down_lr = 0.0 if sparse_plasticity else train_params["learning_rate"]
+        non_top_down_lr = 0.0 if sparse_plasticity else train_params["learning_rate"]
         context_params = [p for n, p in self.network.named_parameters() if "context" in n or "classifier" in n]
         non_context_params = [p for n, p in self.network.named_parameters() if "context" not in n and "classifier" not in n]
         optim = torch.optim.AdamW(
             [
-                {"params": context_params, "lr": top_down_lr, "weight_decay": 0.0},
+                {"params": context_params, "lr": train_params["learning_rate"], "weight_decay": 0.0},
                 {
                     "params": non_context_params,
-                    "lr": train_params["learning_rate"],
+                    "lr": non_top_down_lr,
                     "weight_decay": train_params["weight_decay"],
                 },
             ],
             lr=train_params["learning_rate"], weight_decay=0.0,
         )
+
+        # ensure non-top down parameters are not trainable if sparse is True
+        if sparse_plasticity:
+            for n, p in self.network.named_parameters():
+                if "context" not in n and "classifier" not in n:
+                    p.requires_grad = False
+
 
         self.task = SoftmaxCrossEntropy(
             network=self.network,
@@ -76,89 +86,12 @@ class Train:
             gradient_clip_val=1.0,
             precision=32,
             devices=1,
+            logger=self.logger,
         )
         trainer.fit(self.task, self.train_loader, self.val_loader)
         save_task_info(trainer, self.task_set)
 
         return self.task.network
-
-
-def main(tasks0: List, tasks1: List):
-
-    # initialize random seed
-    np.random.seed(train_params["seed"])
-
-    val_task = ActorCritic if train_params["RL"] else SoftmaxCrossEntropy
-
-    # We will train on a percentage (train_pct) of the tasks in the first round fo training,
-    # and the remainder during the second.
-    # Alternatively, we can specify a holdout_task task for testing
-    task_set = create_task_set(tasks0=tasks0, tasks1=tasks1)
-
-    # add model params from stimulus class
-    add_model_params(task_set)
-
-    # define network
-    network = RNN(**model_params)
-    network = LSTM(**model_params)
-
-    """First round of training"""
-
-    context_params = [p for n, p in network.named_parameters() if "context" in n or "classifier" in n]
-    non_context_params = [p for n, p in network.named_parameters() if "context" not in n and "classifier" not in n]
-    optim = torch.optim.AdamW(
-        [
-            {"params": context_params, "lr": train_params["learning_rate"], "weight_decay": 0.0},
-            {"params": non_context_params, "lr": train_params["learning_rate"], "weight_decay": 1e-3},
-        ],
-        lr=train_params["learning_rate"], weight_decay=0.00,
-    )
-    task = SoftmaxCrossEntropy(
-        network=network,
-        #clssifier_network=Classifers,
-        optim_config=optim,
-        n_logits=model_params["n_output"],
-    )
-
-    train_loader = DataLoader(task_set["train0"], batch_size=train_params["batch_size"], num_workers=train_params["num_workers"])
-    val_loader = DataLoader(task_set["val0"], batch_size=train_params["batch_size"], num_workers=train_params["num_workers"])
-    trainer = pl.Trainer(
-        accelerator='gpu',
-        max_epochs=train_params["num_train_epochs"],
-        gradient_clip_val=1.0,
-        precision=32,
-        devices=1,
-    )
-    save_task_info(trainer, task_set["tasks0"] + task_set["tasks1"])
-
-    trainer.fit(task, train_loader, val_loader)
-
-    """Second round of training"""
-
-    trainable_params = []
-    for name, par in network.named_parameters():
-        if ("context" not in name) and ("value" not in name):
-            par.requires_grad = False
-        else:
-            trainable_params.append(par)
-
-    optim = torch.optim.Adam(trainable_params, train_params["learning_rate"])
-    task = val_task(
-        network=network,
-        optim_config=optim,
-        n_logits=model_params["n_output"],
-    )
-
-    train_loader = DataLoader(task_set["train1"], batch_size=train_params["batch_size"], num_workers=train_params["num_workers"])
-    val_loader = DataLoader(task_set["val1"], batch_size=train_params["batch_size"], num_workers=train_params["num_workers"])
-    trainer = pl.Trainer(
-        accelerator='gpu',
-        max_epochs=train_params["num_test_epochs"],
-        gradient_clip_val=1.0,
-        precision=32,
-        devices=1,
-    )
-    trainer.fit(task, train_loader, val_loader)
 
 
 def add_model_params(task_loader, verbose=False):
@@ -192,83 +125,3 @@ def save_task_info(trainer, tasks):
             tasks_info["stim_offset"].append(-1)
     pickle.dump(tasks_info, open(tasks_fn, "wb"))
 
-
-
-def generate_task_variants(task):
-    task_variants = []
-    for target_offset in [0, 1, 2, 3, 4, 5, 6, 7]:
-        if "delayed_match" in task:
-            for dir_offset in [0, 1, 2]:
-                task_variants.append([task, dir_offset, target_offset])
-        else:
-            task_variants.append([task, target_offset])
-    return task_variants
-
-
-def create_task_set(tasks0: List, tasks1: List, train_pct: float = 0.8):
-
-    task_set = {"tasks0": [], "tasks1": []}
-    print("Task set 0")
-    for t in tasks0:
-        print(t)
-    print("Task set 1")
-    for t in tasks1:
-        print(t)
-
-    for task in tasks0 + tasks1:
-        if task in tasks0 and task not in tasks1:
-            task_set["tasks0"] += generate_task_variants(task)
-        elif task not in tasks0 and task in tasks1:
-            task_set["tasks1"] += generate_task_variants(task)
-        else:
-            t = generate_task_variants(task)
-            n = int(train_pct * len(t))
-            np.random.shuffle(t)
-            task_set["tasks0"] += t[:n]
-            task_set["tasks1"] += t[n:]
-
-    n = len(task_set["tasks0"]) + len(task_set["tasks1"])
-    print(f"Number of tasks in set 1: {len(task_set['tasks0'])}")
-    print(f"Number of tasks in set 2: {len(task_set['tasks1'])}")
-
-    task_set["train0"] = TaskDataset(tasks=task_set['tasks0'], n_total_tasks=n, n_batches=200, RL=train_params["RL"])
-    task_set["val0"] = TaskDataset(tasks=task_set['tasks0'], n_total_tasks=n, n_batches=10, RL=train_params["RL"])
-    task_set["train1"] = TaskDataset(tasks=task_set['tasks1'], n_total_tasks=n, n_batches=200, RL=train_params["RL"])
-    task_set["val1"] = TaskDataset(tasks=task_set['tasks1'], n_total_tasks=n, n_batches=10, RL=train_params["RL"])
-
-    return task_set
-
-def split_train_set(tasks0: List, tasks1: List, train_pct: float=0.9):
-
-    available_tasks = get_available_tasks()
-    train_set_0 = []
-
-
-    if train_pct is not None:
-        n = int(train_pct * len(available_tasks))
-        np.random.shuffle(available_tasks)
-        idx = np.arange(len(available_tasks))
-
-        idx0, idx1 = idx[:n], idx[n:]
-        train_set_0 = [available_tasks[i] for i in idx0]
-        train_set_1 = [available_tasks[i] for i in idx1]
-    else:
-        train_set_0 = [t for t in available_tasks if holdout_task not in t[0]]
-        train_set_1 = [t for t in available_tasks if holdout_task in t[0]]
-
-    return train_set_0, train_set_1
-
-"""
-if __name__ == "__main__":
-    tasks=[2]#,5,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,165]
-    #tasks=[3,4,6,7,8,9,11,12,13,14,15,25,35,45,55,65,75,85,95]
-    seeds = 2
-    #tasks=[2,3]
-    #seeds = 2
-    for j in range(seeds):
-        print('SEED',j)
-        for i in range(len(tasks)):
-            print('TASKS',tasks[i])
-            task=tasks[i]
-            main(tasks=task,seed=j,tasks0=TASKS, tasks1=[])
-"""
